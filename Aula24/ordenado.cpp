b9bc2b345506522de16ec03b48673069fd1508e3
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2018-05-30 20:28:49 -0300

    Exemplo de vector

---
+++ Aula24/ordenado.cpp
@@ -0,0 +1,34 @@
+// PUCRS - Escola Politécnica
+// Programação para Engenharia II
+// 
+// Escreva um programa que recebe números inteiros 
+// como argumentos pela linha
+// de comando e informa se eles estão em ordem.
+//
+// $ ordenado 10 20
+// Ordenado
+// $ ordenado 20 10
+// Desordenado
+// $ ordenado 20 20
+// Ordenado
+// $ ordenado 10 20 30
+// Ordenado
+//
+// marco.mangan@pucrs.br
+// 2018-05-30
+
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+	vector<int> valores;
+	
+	// Completar 
+
+	return 0;
+}
+
+
+
