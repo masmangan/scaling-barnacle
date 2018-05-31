97fe3c552c7f781569c9d69b17f86bfac931a300
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2018-05-30 21:05:11 -0300

    Ordenado

--- Aula24/ordenado.cpp
+++ Aula24/ordenado.cpp
@@ -18,17 +18,62 @@
 // 2018-05-30
 
 #include <vector>
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char* argv[]) 
 {
-	vector<int> valores;
+	// int valores[5];
+	vector<int> valores(argc - 1);
 	
-	// Completar 
+	for (int i = 1 ; i < argc ; i++)
+	{
+		valores[i - 1] = atoi(argv[i]);
+	}
+
+	for (int i = 0 ; i < argc ; i++)
+	{
+		cout << argv[i] << endl;
+	}
+	cout << endl;
+
+	for (size_t i = 0 ; i < valores.size() ; i++)
+	{
+		cout << valores[i] << endl;
+	}
+	cout << endl;
+
+
+
+
+	int ordenado = 1;  // está ordenado... 
+
+	for (size_t i = 1 ; i < valores.size() ; i++)
+	{
+		if (valores[i - 1] > valores[i])
+		{
+			ordenado = 0;
+			break;
+		}
+	}
+
+	if (ordenado == 1)
+	{
+		cout << "Ordenado!" << endl;
+	}
+	else
+	{
+		cout << "Desordenado!" << endl;
+	}
 
 	return 0;
 }
+
+
+
+
 
 
 
